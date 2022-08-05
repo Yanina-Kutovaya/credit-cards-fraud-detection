308078d78441f4353bae56c3bb2b7c5067af516a
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-06 00:49:47 +0300

    Fix get-train-features-dag.py

--- dags/get-train-features-dag.py
+++ dags/get-train-features-dag.py
@@ -5,25 +5,16 @@ from datetime import datetime, timedelta
 
  
 default_args = {
-    'owner': 'airflow',    
-    #'start_date': airflow.utils.dates.days_ago(2),
-    #'end_date': datetime(),
-    #'depends_on_past': False,
-    #'email': ['airflow@example.com'],
-    #'email_on_failure': False,
-    #'email_on_retry': False,
-    # If a task fails, retry it once after waiting at least 5 minutes
-    #'retries': 1,
+    'owner': 'airflow',
     'retry_delay': timedelta(minutes=5)
     }
 dag = DAG(
     dag_id = 'get_train_features_dag',    
     default_args=default_args,
-    start_date=datetime(2022, 8, 1),
-    # schedule_interval='0 0 * * *',
+    start_date=datetime(2022, 8, 1),    
     schedule_interval='@once',
     description='Generate feature_extraction_pipeline and train_features',
-    template_searchpath='/opt/scripts'
+    template_searchpath='/opt/airflow/dags/scripts/'
     )
 print('Generate feature_extraction_pipeline_model and train_features and save them in hdfs')
 generate_model_and_train_features = SparkSubmitOperator(
@@ -38,25 +29,25 @@ copy_model_to_local = BashOperator(
     bash_command='hadoop fs -copyToLocal feature_extraction_pipeline_model_v1 ',
     dag=dag    
     )
-print('Copy  train_features from hdfs to local')
+print('Copy train_features from hdfs to local')
 copy_train_features_to_local = BashOperator(
     task_id='copy_train_features_to_local',
     bash_command='hadoop fs -copyToLocal train_features.parquet ',
     dag=dag    
     )
-print('Copy feature_extraction_pipeline_model_v1 to object storage')
-copy_model_to_object_storage = BashOperator(
-    task_id='copy_model_to_object_storage',
+print('Save feature_extraction_pipeline_model_v1 to object storage')
+save_model_to_object_storage = BashOperator(
+    task_id='save_model_to_object_storage',
     bash_command='aws --endpoint-url=https://storage.yandexcloud.net s3 cp --recursive \
         feature_extraction_pipeline_model_v1 s3://credit-cards-data/feature_extraction_pipeline_model_v1/',
     dag=dag    
     )
-print('Copy train_features to object storage')
-copy_train_features_to_object_storage = BashOperator(
-    task_id='copy_train_features_to_object_storage',
+print('Save train_features to object storage')
+save_train_features_to_object_storage = BashOperator(
+    task_id='save_train_features_to_object_storage',
     bash_command='aws --endpoint-url=https://storage.yandexcloud.net s3 cp --recursive \
         train_features.parquet s3://credit-cards-data/train_features.parquet',
     dag=dag    
     )
-generate_model_and_train_features >> copy_model_to_local >> copy_model_to_object_storage
-generate_model_and_train_features >> copy_train_features_to_local >> copy_train_features_to_object_storage
+generate_model_and_train_features >> copy_model_to_local >> save_model_to_object_storage
+generate_model_and_train_features >> copy_train_features_to_local >> save_train_features_to_object_storage
