b8004701a614e0dcd44e84f908556cf838979253
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-05 17:51:46 +0300

    Add generate_train_features.py

--- dags/scripts/generate_train_features.py
+++ dags/scripts/generate_train_features.py
@@ -1,23 +1,25 @@
-app_name = 'train_data_preprocessing'
-spark_ui_port = 4041
-
-import pyspark
-from pyspark import SparkFiles
+app_name = 'Train data preprocessing' 
+import sys
+ 
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SQLContext
 from scripts.feature_extraction_pipeline_v1 import get_feature_extraction_pipeline
-from scripts.s3_operator import S3_operator as op
+ 
+def main():
+    conf = SparkConf().setAppName('app_name')
+    conf.set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider")
+    sc = SparkContext(conf=conf)
+ 
+    sql = SQLContext(sc)
+    df_train = sql.read.parquet('s3a://credit-cards-data/train.parquet')
+    defaultFS = sc._jsc.hadoopConfiguration().get('fs.defaultFS')
 
-spark = (
-    pyspark.sql.SparkSession.builder
-        .appName(app_name)        
-        .config("spark.ui.port", spark_ui_port)
-        .getOrCreate()
-        )
-df_train = op.get_object('train.parquet').repartition(8)
-feature_extraction_pipeline = get_feature_extraction_pipeline()
-feature_extraction_pipeline_model = feature_extraction_pipeline.fit(df_train)
-df_train = feature_extraction_pipeline_model.transform(df_train)
+    feature_extraction_pipeline = get_feature_extraction_pipeline()
+    feature_extraction_pipeline_model = feature_extraction_pipeline.fit(df_train)
+    df_train = feature_extraction_pipeline_model.transform(df_train)
 
-feature_extraction_pipeline_model.save('feature_extraction_pipeline_model_v1')
-df_train.write.parquet('train_features.parquet')
-op.send_object('feature_extraction_pipeline_model_v1')
-op.send_object('train_features.parquet')   
+    feature_extraction_pipeline_model.save(defaultFS+'/tmp/feature_extraction_pipeline_model_v1')
+    df_train.write.parquet(defaultFS+'/tmp/train_features.parquet')    
+ 
+if __name__ == "__main__":
+    main()
