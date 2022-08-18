c444d95ecd43a2ce1920820d2d0c5b3a08704ee8
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-18 19:29:18 +0300

    Fix get-feature-extraction-model.py

--- dags/get-feature-extraction-model.py
+++ dags/get-feature-extraction-model.py
@@ -1,8 +1,6 @@
 MODEL = 'feature_extraction_pipeline_model'
 YC_S3 = 'aws s3 --endpoint-url=https://storage.yandexcloud.net'
-YC_INPUT_DATA_BUCKET = 'airflow-cc-input'   # S3 bucket for input data
 YC_OUTPUT_DATA_BUCKET = 'airflow-cc-output' # S3 bucket for output data
-YC_SOURCE_BUCKET = 'airflow-cc-source'      # S3 bucket for pyspark source files
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
@@ -21,19 +19,9 @@ dag = DAG(
     schedule_interval='@once',
     description='Generate feature_extraction_pipeline_model and train_features',       
 )
-copy_train_from_s3 = BashOperator(
-    task_id='copy_train_from_s3',
-    bash_command=f'{YC_S3} cp s3://{YC_INPUT_DATA_BUCKET}/train.csv .  ',
-    dag=dag    
-)
-put_train_to_hdfs = BashOperator(
-    task_id='put_train_to_hdfs',
-    bash_command='hdfs dfs -put train.csv ',
-    dag=dag    
-)
 generate_model_and_train_features = SparkSubmitOperator(
     task_id='generate_model_and_train_features',
-    application = 'airflow/dags/scripts/feature_extraction_model.py',    
+    application = 'airflow/dags/scripts/generate_train_features.py',    
     dag=dag
 )
 copy_model_to_local = BashOperator(
@@ -58,6 +46,5 @@ save_train_features_to_s3 = BashOperator(
         s3://{YC_OUTPUT_DATA_BUCKET}/train_features.parquet ',
     dag=dag    
 )
-copy_train_from_s3 >> put_train_to_hdfs >> generate_model_and_train_features
 generate_model_and_train_features >> copy_model_to_local >> save_model_to_s3
 generate_model_and_train_features >> copy_train_features_to_local >> save_train_features_to_s3
