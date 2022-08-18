cb8a445cd08639ce910721b68a2e09944a2bc968
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-18 10:08:44 +0300

    Fix DATA_INGEST.py

--- dags/DATA_INGEST.py
+++ dags/DATA_INGEST.py
@@ -61,7 +61,7 @@ with DAG(
         # Pre-created persistent light cluster with HDFS, HIVE, MAPREDUCE, YARN
         # from MASTERNODE and DATANODE
         'spark:spark.hive.matastore.uris': 'thrift://rc1a-dataproc-m-zqbxjx006jc7qwiq.mdb.yandexcloud.net:9083',
-        'spark:spark.hive.matastore.warehose.dir:': 's3a://dataproc-1/metastore/'
+        'spark:spark.hive.matastore.warehose.dir': 's3a://dataproc-1/metastore/'
         },
         connection_id='yc-airflow-sa',
         dag=ingest_dag
