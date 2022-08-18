a3c821df5c7560050bdd7ebe3c9204401b0bb1ab
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-18 15:35:34 +0300

    Fix generate_test_features.py

--- dags/scripts/generate_test_features.py
+++ dags/scripts/generate_test_features.py
@@ -1,21 +1,30 @@
-app_name = 'Test data preprocessing' 
-import sys
- 
-from pyspark import SparkContext, SparkConf
-from pyspark.sql import SQLContext
+APP_NAME = 'Test data preprocessing'
+YC_INPUT_DATA_BUCKET = 'airflow-cc-input'   # S3 bucket for input data
+YC_SOURCE_BUCKET = 'airflow-cc-source'      # S3 bucket for pyspark source files
+
+from pyspark.sql import SparkSession
+from custom_transformers import *
 from pyspark.ml import PipelineModel
- 
+
+
 def main():
-    conf = SparkConf().setAppName(app_name)
-    conf.set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider")
-    sc = SparkContext(conf=conf)
- 
-    sql = SQLContext(sc)
-    df_test = sql.read.parquet('s3a://credit-cards-data/test.parquet')          
-    defaultFS = sc._jsc.hadoopConfiguration().get('fs.defaultFS')
-    feature_extraction_pipeline_model = PipelineModel.load('feature_extraction_pipeline_model_v1')        
-    df_test = feature_extraction_pipeline_model.transform(df_test)         
-    df_test.write.parquet(defaultFS+'/tmp/test_features.parquet')    
- 
+    spark = (
+        SparkSession.builder
+        .appName(APP_NAME)
+        .enableHiveSupport()
+        .getOrCreate()
+    )
+    df_test = (
+        spark.read.format('csv')
+        .option('headers', True)
+        .option('delimiter', ',')
+        .load('test.csv')
+    )
+    df_test = df_test.repartition(4)
+    feature_extraction_pipeline_model = PipelineModel.load('feature_extraction_pipeline_model')
+    df_test = feature_extraction_pipeline_model.transform(df_test)
+    df_test.repartition(1).write.format('parquet').save('test_features.parquet')
+
+
 if __name__ == "__main__":
     main()
