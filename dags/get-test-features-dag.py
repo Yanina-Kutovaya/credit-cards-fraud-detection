548f2eb002fbb2ec62b4a819852a8b6d577bafce
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-19 23:02:40 +0300

    Fix get-test-features-dag.py

--- dags/get-test-features-dag.py
+++ dags/get-test-features-dag.py
@@ -1,8 +1,5 @@
-MODEL = 'feature_extraction_pipeline_model'
 YC_S3 = 'aws s3 --endpoint-url=https://storage.yandexcloud.net'
-YC_INPUT_DATA_BUCKET = 'airflow-cc-input'   # S3 bucket for input data
 YC_OUTPUT_DATA_BUCKET = 'airflow-cc-output' # S3 bucket for output data
-YC_SOURCE_BUCKET = 'airflow-cc-source'      # S3 bucket for pyspark source files
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
@@ -14,12 +11,7 @@ dag = DAG(
     dag_id = 'get_test_features_dag',
     start_date=datetime(2022, 8, 15),
     schedule_interval='@daily',
-    description='Generate test_features from feature_extraction_pipeline_model and test.parquet'    
-)
-get_model_from_s3 = BashOperator(
-    task_id='get_model_from_s3',
-    bash_command = f'{YC_S3} cp s3://{YC_OUTPUT_DATA_BUCKET}/{MODEL} . ',
-    dag=dag 
+    description='Generate test_features from feature_extraction_pipeline_model and test data'    
 )
 generate_test_features = SparkSubmitOperator(
     task_id='generate_test_features',
