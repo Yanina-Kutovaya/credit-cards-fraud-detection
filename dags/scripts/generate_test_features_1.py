1e0a5802734e7f965004e3d11604f899387147fb
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-20 02:02:14 +0300

    Add generate_test_features_1.py

--- dags/scripts/generate_test_features_1.py
+++ dags/scripts/generate_test_features_1.py
@@ -1,8 +1,6 @@
 APP_NAME = 'Test data preprocessing'
-YC_INPUT_DATA_BUCKET = 'airflow-cc-input'   # S3 bucket for input data
 
 from pyspark.sql import SparkSession
-from pyspark import SparkFiles
 from custom_transformers import (
     DiscreteToBinaryTransformer,
     ContinuousOutliersCapper,
@@ -13,21 +11,11 @@ from custom_transformers import (
 from pyspark.ml import PipelineModel
 
 
-def main(): 
-    spark = (
-        SparkSession.builder
-        .appName(APP_NAME)
-        .enableHiveSupport()
-        .getOrCreate()    
-    )
-    TEST_DATA_URL = f'https://storage.yandexcloud.net/{YC_INPUT_DATA_BUCKET}/test.csv'
-
-    spark.sparkContext.addFile(TEST_DATA_URL)
-    df_test = spark.read.parquet(
-        SparkFiles.get('test.parquet'), header=True, inferSchema= True
-    )    
+def main():
+    spark = SparkSession.builder.appName(APP_NAME).getOrCreate()    
+    df_test = spark.read.parquet('test.parquet')
     df_test = df_test.repartition(4)
-    feature_extraction_pipeline_model = PipelineModel.load('feature_extraction_pipeline_model')
+    feature_extraction_pipeline_model = PipelineModel.load('feature_extraction_pipeline_model')      
     df_test = feature_extraction_pipeline_model.transform(df_test)
     df_test.repartition(1).write.format('parquet').save('test_features.parquet')
 
