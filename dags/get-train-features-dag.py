df3a3df867f1f27cea0d8f3a32637c0811f8eb88
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-20 12:23:51 +0300

    Fix get-train-features-dag.py

--- dags/get-train-features-dag.py
+++ dags/get-train-features-dag.py
@@ -1,51 +1,51 @@
+MODEL = 'feature_extraction_pipeline_model'
+YC_S3 = 'aws s3 --endpoint-url=https://storage.yandexcloud.net'
+YC_SOURCE_BUCKET = 'airflow-cc-source'      # S3 bucket for pyspark source files
+YC_OUTPUT_DATA_BUCKET = 'airflow-cc-output' # S3 bucket for output data
+
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator 
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime, timedelta
 
  
-default_args = {
-    'owner': 'airflow',
-    'retry_delay': timedelta(minutes=5)
-    }
 dag = DAG(
-    dag_id = 'get_train_features_dag',    
-    default_args=default_args,
-    start_date=datetime(2022, 8, 1),    
+    dag_id = 'get_train_features_dag',
+    start_date=datetime(2022, 8, 15),
     schedule_interval='@once',
-    description='Generate feature_extraction_pipeline and train_features',       
-    )
-print('Generate feature_extraction_pipeline_model and train_features and save them in hdfs')
+    description='Generate feature_extraction_pipeline_model and train_features'    
+)
+copy_custom_transformers_from_s3 = BashOperator(
+    task_id='copy_custom_transformers_from_s3',
+    bash_command=f'{YC_S3} cp s3://{YC_SOURCE_BUCKET}/custom_transformers.py /home/ubuntu/ ',
+    dag=dag    
+)
+copy_pipeline_from_s3 = BashOperator(
+    task_id='copy_pipeline_from_s3',
+    bash_command=f'{YC_S3} cp s3://{YC_SOURCE_BUCKET}/feature_extraction_pipeline.py /home/ubuntu/ ',
+    dag=dag    
+)
+copy_script_from_s3 = BashOperator(
+    task_id='copy_script_from_s3',
+    bash_command=f'{YC_S3} cp s3://{YC_SOURCE_BUCKET}/feature_extraction_model.py /home/ubuntu/ ',
+    dag=dag    
+)
 generate_model_and_train_features = SparkSubmitOperator(
     task_id='generate_model_and_train_features',
-    application = 'airflow/dags/scripts/generate_train_features.py',    
+    application = '/home/ubuntu/feature_extraction_model.py',
     dag=dag
-    )
-print('Copy feature_extraction_pipeline_model_v1 from hdfs to local')
-copy_model_to_local = BashOperator(
-    task_id='copy_model_to_local',
-    bash_command='hadoop fs -copyToLocal feature_extraction_pipeline_model_v1 ',
-    dag=dag    
-    )
-print('Copy train_features from hdfs to local')
-copy_train_features_to_local = BashOperator(
-    task_id='copy_train_features_to_local',
-    bash_command='hadoop fs -copyToLocal train_features.parquet ',
-    dag=dag    
-    )
-print('Save feature_extraction_pipeline_model_v1 to object storage')
-save_model_to_object_storage = BashOperator(
-    task_id='save_model_to_object_storage',
-    bash_command='aws --endpoint-url=https://storage.yandexcloud.net s3 cp --recursive \
-        feature_extraction_pipeline_model_v1 s3://credit-cards-data/feature_extraction_pipeline_model_v1/ ',
-    dag=dag    
-    )
-print('Save train_features to object storage')
-save_train_features_to_object_storage = BashOperator(
-    task_id='save_train_features_to_object_storage',
-    bash_command='aws --endpoint-url=https://storage.yandexcloud.net s3 cp --recursive \
-        train_features.parquet s3://credit-cards-data/train_features.parquet ',
-    dag=dag    
-    )
-generate_model_and_train_features >> copy_model_to_local >> save_model_to_object_storage
-generate_model_and_train_features >> copy_train_features_to_local >> save_train_features_to_object_storage
+)
+save_model_to_s3 = BashOperator(
+    task_id='save_model_to_s3',
+    bash_command = f'{YC_S3} cp --recursive {MODEL} \
+        s3://{YC_OUTPUT_DATA_BUCKET}/{MODEL} ',
+    dag=dag
+)
+save_train_features_to_s3 = BashOperator(
+    task_id='save_train_features_to_s3',
+    bash_command = f'{YC_S3} cp --recursive train_features.parquet \
+        s3://{YC_OUTPUT_DATA_BUCKET}/train_features.parquet ',
+    dag=dag
+)
+[copy_custom_transformers_from_s3, copy_pipeline_from_s3, copy_script_from_s3] >> generate_model_and_train_features 
+generate_model_and_train_features >> [save_model_to_s3, save_train_features_to_s3]
