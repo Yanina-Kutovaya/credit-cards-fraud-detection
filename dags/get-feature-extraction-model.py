1ba0c65756d4eb427d653c01ed4548dcc79036b4
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-21 10:40:54 +0300

    Fix get-feature-extraction-model

--- dags/get-feature-extraction-model.py
+++ dags/get-feature-extraction-model.py
@@ -19,9 +19,24 @@ dag = DAG(
     schedule_interval='@once',
     description='Generate feature_extraction_pipeline_model and train_features',       
 )
+copy_custom_transformers_from_s3 = BashOperator(
+        task_id='copy_custom_transformers_from_s3',
+        bash_command=f'{YC_S3} cp s3://{YC_SOURCE_BUCKET}/custom_transformers.py /home/ubuntu/ ',
+        dag=dag
+)
+copy_pipline_from_s3 = BashOperator(
+        task_id='copy_pipline_from_s3',
+        bash_command=f'{YC_S3} cp s3://{YC_SOURCE_BUCKET}/feature_extraction_pipeline.py /home/ubuntu/ ',
+        dag=dag
+)
+copy_script_from_s3 = BashOperator(
+        task_id='copy_script_from_s3',
+        bash_command=f'{YC_S3} cp s3://{YC_SOURCE_BUCKET}/generate_train_features.py /home/ubuntu/ ',
+        dag=dag
+)
 generate_model_and_train_features = SparkSubmitOperator(
     task_id='generate_model_and_train_features',
-    application = 'airflow/dags/scripts/generate_train_features.py',    
+    application = '/home/ubuntu/generate_train_features.py',    
     dag=dag
 )
 copy_model_to_local = BashOperator(
@@ -46,5 +61,6 @@ save_train_features_to_s3 = BashOperator(
         s3://{YC_OUTPUT_DATA_BUCKET}/train_features.parquet ',
     dag=dag    
 )
+[copy_custom_transformers_from_s3, copy_pipline_from_s3, copy_script_from_s3]>> generate_model_and_train_features 
 generate_model_and_train_features >> copy_model_to_local >> save_model_to_s3
 generate_model_and_train_features >> copy_train_features_to_local >> save_train_features_to_s3
