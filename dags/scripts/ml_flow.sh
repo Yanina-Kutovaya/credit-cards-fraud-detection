1bb5136a0390475965132daa0e02cfb1ba41246f
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-22 13:55:09 +0300

    Fix ml_flow.sh

--- dags/scripts/ml_flow.sh
+++ dags/scripts/ml_flow.sh
@@ -2,4 +2,4 @@ spark-submit \
 --jars mlflow-spark-1.27.0.jar \
 ml_flow.py \
 --train_artifact "s3a://airflow-cc-input/train.csv" \
---output_artifact fraud_detection_model
+--output_artifact Fraud_Detection_Model_Pipline
