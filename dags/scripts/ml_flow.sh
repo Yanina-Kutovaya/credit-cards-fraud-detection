896fd11052157de1fa44220a0eb251c2dc54c394
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-24 15:54:54 +0300

    Fix ml_flow.sh

--- dags/scripts/ml_flow.sh
+++ dags/scripts/ml_flow.sh
@@ -1,5 +1,5 @@
 spark-submit \
 --jars mlflow-spark-1.27.0.jar \
 ml_flow.py \
---train_artifact "s3a://airflow-cc-input/train.csv" \
---output_artifact Fraud_Detection_Model_Pipline
+--train_artifact 's3a://airflow-cc-input/train.parquet' \
+--output_artifact 'Spark_GBTClassifier_v1'
