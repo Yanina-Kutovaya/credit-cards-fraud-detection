eb53b97d6b9cf93dfeb7c55852f99bc6498c0d5d
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-24 18:18:23 +0300

    Fix ml-flow-dag.py

--- dags/ml-flow-dag.py
+++ dags/ml-flow-dag.py
@@ -1,4 +1,6 @@
-MODEL = 'feature_extraction_pipeline_model'
+TRAIN_ARTIFACT = 'train.parquet'
+OUTPUT_ARTIFACT = 'Spark_GBTClassifier_v1'
+
 YC_S3 = 'aws s3 --endpoint-url=https://storage.yandexcloud.net'
 YC_SOURCE_BUCKET = 'airflow-cc-source'      # S3 bucket for pyspark source files
 YC_INPUT_DATA_BUCKET = 'airflow-cc-input'   # S3 bucket for input data
@@ -11,13 +13,25 @@ from airflow.operators.bash import BashOperator
 
 
 with DAG(
-    dag_id = 'get_test_features_dag',
-    schedule_interval='@monthly',
-    start_date=datetime(2022, 8, 15),
+    dag_id = 'ml_flow_dag',
+    schedule_interval='@daily',
+    start_date=datetime(2022, 7, 15),
     max_active_runs=1,
-    catchup=False
+    catchup=True
     ) as ingest_dag:
 
+    copy_data_from_s3 = BashOperator(
+        task_id='copy_data_from_s3',
+        bash_command=(
+            f'{YC_S3} cp s3://{YC_INPUT_DATA_BUCKET}/{TRAIN_ARTIFACT} '
+            '/home/ubuntu/train.parquet '
+        )   
+    )
+    move_data_to_hdfs = BashOperator(
+        task_id='move_data_to_hdfs',
+        bash_command = f'hdfs dfs -moveFromLocal /home/ubuntu/{TRAIN_ARTIFACT} '
+
+    )
     copy_script_from_s3 = BashOperator(
         task_id='copy_script_from_s3',
         bash_command=(
@@ -38,27 +52,30 @@ with DAG(
             f'{YC_S3} cp s3://{YC_SOURCE_BUCKET}/fraud_detection_model_pipeline.py '
             f'/home/ubuntu/ '
         )   
-    )
+    )    
     train_model = BashOperator(
         task_id='train_model',
         bash_command=(
             'spark-submit '
             '/home/ubuntu/ml_flow.py '            
-            f'--train_artifact "s3a://{YC_INPUT_DATA_BUCKET}/train.parquet" '
-            '--output_artifact "Spark_GBTClassifier_v1" '            
+            f'--train_artifact "{TRAIN_ARTIFACT}" '
+            f'--output_artifact "{OUTPUT_ARTIFACT}" '            
         ),         
     )
     copy_model_to_local = BashOperator(
         task_id='copy_model_to_local',
         bash_command=(
-            'hdfs dfs -copyToLocal Spark_GBTClassifier_v1 '
-            '/home/ubuntu/Spark_GBTClassifier_v1 '
+            f'hdfs dfs -copyToLocal {OUTPUT_ARTIFACT} '
+            f'/home/ubuntu/{OUTPUT_ARTIFACT} '
         )
     )
     copy_model_to_s3 = BashOperator(
        task_id='copy_model_to_s3',
        bash_command=(
-        f'{YC_S3} cp /home/ubuntu/Spark_GBTClassifier_v1/ '
-        f's3://{YC_OUTPUT_DATA_BUCKET}/Spark_GBTClassifier_v1 --recursive '        
+        f'{YC_S3} cp /home/ubuntu/{OUTPUT_ARTIFACT}/ '
+        f's3://{YC_OUTPUT_DATA_BUCKET}/{OUTPUT_ARTIFACT} --recursive '        
        )
     )
+    copy_data_from_s3 >> move_data_to_hdfs >> train_model
+    [copy_script_from_s3, copy_custom_transformers_from_s3, copy_fraud_detection_pipeline_from_s3] >> train_model
+    train_model >> copy_model_to_local >> copy_model_to_s3
