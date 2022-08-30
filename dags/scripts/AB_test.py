7c9e0c4daca29b4957464e9f32d17e7505d5da75
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-08-30 15:06:37 +0300

    Fix AB_test.py

--- dags/scripts/AB_test.py
+++ dags/scripts/AB_test.py
@@ -1,40 +1,20 @@
-APP_NAME = 'Model Validation'
+APP_NAME = 'Model Automated Retraining and Validation'
 YC_INPUT_DATA_BUCKET = 'airflow-cc-input'
 YC_OUTPUT_DATA_BUCKET = 'airflow-cc-output'
 YC_SOURCE_BUCKET = 'airflow-cc-source'
 
-import requests
-
-CUSTOM_TRANSFORMERS_URL = f'https://storage.yandexcloud.net/{YC_SOURCE_BUCKET}/custom_transformers.py'
-FRAUD_DETECTION_MODEL_PIPELINE_URL = f'https://storage.yandexcloud.net/{YC_SOURCE_BUCKET}/fraud_detection_model_pipeline.py'
-TRAIN_DATA_URL = f'https://storage.yandexcloud.net/{YC_INPUT_DATA_BUCKET}/train.parquet'
-
-r = requests.get(CUSTOM_TRANSFORMERS_URL, allow_redirects=True)
-open('custom_transformers.py', 'wb').write(r.content)
-
-r = requests.get(FRAUD_DETECTION_MODEL_PIPELINE_URL, allow_redirects=True)
-open('fraud_detection_model_pipeline.py', 'wb').write(r.content)
-
-r = requests.get(TRAIN_DATA_URL, allow_redirects=True)
-open('train.parquet', 'wb').write(r.content)
-
-
-import numpy as np
-import pandas as pd
 import logging
 import argparse
-from datetime import datetime
-from pyspark import SparkFiles
+import mlflow
+import pandas as pd
+import numpy as np
+from scipy.stats import norm, ttest_ind
+
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
-from pyspark.sql.types import IntegerType, DoubleType
-
-import mlflow
+from pyspark.sql.types import DoubleType
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import GBTClassifier
-
-from scipy.stats import norm
-import seaborn as sns
 
 from custom_transformers import (
     DiscreteToBinaryTransformer,
@@ -45,11 +25,11 @@ from custom_transformers import (
 )
 from fraud_detection_model_pipeline import get_fraud_detection_model_pipeline
 
+
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
-def get_train_valid_dataset(data):
-    logger.info('Train validation split ...')
+def train_validation_split(data):
     t = 3600 * 24 * 30
     train = data.filter(F.col('TransactionDT') < t)
     compromised_cards = (
@@ -62,14 +42,6 @@ def get_train_valid_dataset(data):
     )
     return train, valid
 
-
-def get_model(classifier, inf_pipeline, train):
-    stages = inf_pipeline.getStages()[:-1]
-    stages.append(classifier)
-    pipeline = Pipeline(stages=stages)
-    model = pipeline.fit(train)
-  
-    return model
 
 def get_cards_precision_top_k(predictions, k=50, bootstrap_iterations=100, seed=25):
     np.random.seed(seed)
@@ -103,62 +75,102 @@ def get_cards_precision_top_k(predictions, k=50, bootstrap_iterations=100, seed=
     precision_std = scores[f'precision_top_{k}'].std()
     precision_low = precision_mean - 3 * precision_std
     precision_upp = precision_mean + 3 * precision_std
-    logger.info(f'CP@{k} = {round(precision_mean, 4)} +/- {round(precision_std, 4)}\n')
-    
+    logger.info(f'CP@{k} = {round(precision_mean, 4)} +/- {round(precision_std, 4)}\n')    
+
     return scores
 
 
-def main():
-    np.random.seed(18)
-    # Create Spark Session
+def main(args):
     logger.info('Creating Spark Session ...')
     spark = SparkSession.builder.appName(APP_NAME).getOrCreate()
 
-    # Load data
     logger.info('Loading data ...')
-    data_path = 'train.parquet'
+    data_path = args.train_artifact
     data = spark.read.parquet(data_path, header=True, inferSchema=True)
 
-    logger.info('Splitting data into train and validation dataset ...')
-    train, valid = get_train_valid_dataset(data)
+    logger.info('Train validation split ...')
+    train, valid = train_validation_split(data)
 
+    logger.info('Loading saved model ...')
+    saved_model_path = args.saved_artifact
+    loaded_model = mlflow.spark.load_model(saved_model_path)
+
+    logger.info('Scoring saved model ...') 
+    predictions_A = loaded_model.transform(valid)
+    scores_A = get_cards_precision_top_k(predictions_A)
+
+    logger.info("Building new inference pipeline ...")
     inf_pipeline = get_fraud_detection_model_pipeline()
     classifier_B = GBTClassifier(
-        featuresCol='features',
-        labelCol='isFraud',
-        maxDepth = 10,
-        minInstancesPerNode=100
+            featuresCol='features',
+            labelCol='isFraud',
+            maxDepth = 6,
+            minInstancesPerNode=1000
     )
-    logger.info("Fitting inference pipeline B ...")
-    model_B = get_model(classifier_B, inf_pipeline, train)
+    stages = inf_pipeline.getStages()[:-1]
+    stages.append(classifier_B)
+    new_pipeline = Pipeline(stages=stages)
+
+    logger.info("Fitting new inference pipeline ...")
+    model_B = new_pipeline.fit(train)
 
     logger.info('Scoring model B ...')  
     predictions_B = model_B.transform(valid)
-    
-    logger.info("A/B testing ...")
-    scores_A = pd.read_csv('scores_Spark_GBTClassifier_v1.csv')
     scores_B = get_cards_precision_top_k(predictions_B)
 
-    from scipy.stats import ttest_ind
-
-    k = 50
-    alpha = 0.05
-
+    logger.info('Performing A/B test ...')
+    k = args.k_cards    
     pvalue = ttest_ind(
         scores_A[f'precision_top_{k}'], 
         scores_B[f'precision_top_{k}'], 
         alternative="less"
     ).pvalue
-
-    print(f"pvalue: {pvalue: g}")
-    if pvalue < alpha:
-        logger.info("Reject null hypothesis.")
-        logger.info("Saving inference pipeline B ...")
-        mlflow.spark.save_model(model_B, 'Spark_GBTClassifier_v2')
-        scores_B.to_csv('scores_Spark_GBTClassifier_v2.csv', index=False) 
+    logger.info(f'pvalue: {pvalue: g}')    
+    if pvalue < args.p_value:
+        logger.info('Reject null hypothesis.')        
+        logger.info("Saving inference pipeline B ...")        
+        model_B.save(args.output_artifact)     
     else:
-        logger.info("Accept null hypothesis.")
+        logger.info('Accept null hypothesis.')       
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description='Model (Inference Pipline) Training and Validation'
+    )
+    # For CLI use f's3a://airflow-cc-input/train.csv'
+    parser.add_argument(
+        "--train_artifact", 
+        type=str,
+        help='Fully qualified name for training artifact/dataset' 
+        'Training dataset will be split into train and validation',
+        required=True
+    )
+    parser.add_argument(
+        "--k_cards",
+        type=int,
+        default = 50,
+        help="Number of cards for precision top k calculation.",
+    )
+    parser.add_argument(
+        "--p_value",
+        type=float,
+        default = 0.01,
+        help="p-value for A/B test model selection.",
+    )
+    # For CLI use 'Spark_GBTClassifier_v1'
+    parser.add_argument(
+        "--saved_artifact",
+        type=str,
+        help="Name for the saved serialized model (Inference Artifact folder)",
+        required=True,
+    )
+    # For CLI use 'Spark_GBTClassifier_v2'
+    parser.add_argument(
+        "--output_artifact",
+        type=str,
+        help="Name for the output serialized model (Inference Artifact folder)",
+        required=True,
+    )
+    args = parser.parse_args()
+    main(args)
