f019d2a015bddfdf1c4c547955bc8ff6f421b674
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-09-09 06:24:23 +0300

    Fix kafka_producer.py

--- dags/scripts/kafka_producer.py
+++ dags/scripts/kafka_producer.py
@@ -45,8 +45,7 @@ def main():
         default='test.parquet',
         type=str, 
         help='Fully qualified name for testing dataset'
-    )    
-    
+    )
     args = argparser.parse_args()
 
     producer = kafka.KafkaProducer(
@@ -57,10 +56,15 @@ def main():
         sasl_plain_password='Moscow117279',
         ssl_cafile='/usr/local/share/ca-certificates/Yandex/YandexCA.crt',
         value_serializer=serialize,
+    )
+    APP_NAME = 'Generate test data'
+    spark = SparkSession.builder.appName(APP_NAME).getOrCreate()    
+    data = spark.read.parquet(
+        args.data_artifact, header=True, inferSchema=True
     )    
     try:
         for i in range(args.n):
-            record_md = send_message(args.data_artifact, producer, args.topic)
+            record_md = send_message(data, producer, args.topic)
             print(
                 f'Msg sent. Topic: {record_md.topic}, partition:{record_md.partition}, offset:{record_md.offset}'
             )
@@ -70,8 +74,8 @@ def main():
     producer.close()
 
 
-def send_message(data_artifact_path: str, producer: kafka.KafkaProducer, topic: str) -> RecordMetadata:
-    generated_data = generate_data(data_artifact_path)
+def send_message(data, producer: kafka.KafkaProducer, topic: str) -> RecordMetadata:
+    generated_data = generate_data(data)
     features = producer.send(
         topic=topic,
         key=str(generated_data['card_id']).encode('ascii'),
@@ -86,12 +90,7 @@ def send_message(data_artifact_path: str, producer: kafka.KafkaProducer, topic: 
     )
 
 
-def generate_data(data_artifact_path) -> Dict:
-    APP_NAME = 'Generate test data'
-    spark = SparkSession.builder.appName(APP_NAME).getOrCreate()    
-    data = spark.read.parquet(
-        data_artifact_path, header=True, inferSchema=True
-    )
+def generate_data(data) -> Dict:    
     df = data.sample(0.0001).toPandas()
     card_id = int(df.loc[0, 'card1'])
     card_data = df.iloc[:1, :].to_json()
